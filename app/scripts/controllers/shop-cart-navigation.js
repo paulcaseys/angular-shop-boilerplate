bc7a5ca6575faa23313691a336a12e92011e97ba
Paul Casey <paulcaseys@gmail.com>
2016-01-11 16:00:39 +1100

    adding cart dropdown

--- app/scripts/controllers/shop-cart-navigation.js
+++ app/scripts/controllers/shop-cart-navigation.js
@@ -13,12 +13,15 @@ angular.module('angularBoilerplateApp')
 	$scope.cartItems = CartProvider.getCartItems();
 	$scope.cartItemsTotalItems = CartProvider.getCartItemsTotalItems();
 	$scope.cartItemsTotalPrice = CartProvider.getCartItemsTotalPrice();
+    $scope.cartItemsMessage = "there are currently no items in your cart";
 	
     var updatedCart = function(){
 		console.log("CART UPDATED");
 		$scope.cartItems = CartProvider.getCartItems();
 		$scope.cartItemsTotalItems = CartProvider.getCartItemsTotalItems();
 		$scope.cartItemsTotalPrice = CartProvider.getCartItemsTotalPrice();
+
+        // displays message in dropdown
 	};
 
 	// observes provider for updates
